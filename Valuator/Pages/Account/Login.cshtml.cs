05619d1f08ca1e0ee0a86382171f205949031cf4
wqrxysf <wqrxysf@yandex.ru>
2026-05-20 14:33:55 +0300

    pa7

--- Valuator/Pages/Account/Login.cshtml.cs
+++ Valuator/Pages/Account/Login.cshtml.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Valuator.Services;
